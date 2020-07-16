26ef1fe60163980a9c15ef1f158e737071696f7b
Amanda Walker <walker.akw@gmail.com>
2020-07-16 04:51:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,8 +9,8 @@ attr_accessor :roster
   def add_student(name, grade)
 
       if @roster[grade] == nil
-        @roster[grade] = []
-        @roster[grade] << name
+        #@roster[grade] = []
+        @roster[grade] = [name]
       else
         @roster[grade] << name
       end #if
